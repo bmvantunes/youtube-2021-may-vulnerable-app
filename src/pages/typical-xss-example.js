e70ed0a349d842f8433bd93f2561ceb0e8b3a603
Bruno Antunes <bmvantunes@gmail.com>
2021-05-31 20:24:22 +0100

    add ebay

--- src/pages/typical-xss-example.js
+++ src/pages/typical-xss-example.js
@@ -9,11 +9,12 @@ export default function TypicalXssExample() {
   // you can try setting the textbox to:
   // <img src="this-image-does-not-exist.png" onerror="console.log(localStorage);" style="display:none;"/>
   // or navigate to:
-  // http://localhost:3000/typical-xss-example?initial=%3Cimg%20src=%22this-doesnot-exist.png%22%20onerror=%22console.log(localStorage);%22%20style=%22display:none;%22/%3E
+  // http://localhost:3000/typical-xss-example?initial=%3Cimg%20src=%22this-doesnot-exist.png%22%20onerror=%22console.log('I can run any code LOL', localStorage);%22%20style=%22display:none;%22/%3E
 
   return (
     <>
-      <h2>Your Comment</h2>
+      <h1>Welcome to eBay</h1>
+      <h2>Your product description:</h2>
       <textarea
         onChange={(evt) => setComment(evt.target.value)}
         value={commentOrQuery}
